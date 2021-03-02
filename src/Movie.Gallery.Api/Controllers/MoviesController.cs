da85aff124e8578160de4dc0ec76f5dad433f5a9
Nadia Regina De Oliveira <nadia.oliveira@easynvest.com.br>
2021-03-01 23:11:29 -0300

    Cria classe de filmes

---
+++ src/Movie.Gallery.Api/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Movie.Gallery.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Movie.Gallery.Application/Queries/GetMovies/GetMovieQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Movie.Gallery.Application.Queries.GetMovies
+{
+    public struct GetMovieQuery
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Direction { get; set; }
+        public DateTime Year { get; set; }
+        public int Duration { get; set; }
+    }
+}
